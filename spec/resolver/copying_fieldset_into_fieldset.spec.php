cf3a0eef486657a5a79fa3998c9d39d14be4cf5b
Simon Repp <simon@fdpl.io>
2018-10-27 15:25:39 +0200

    Add complete specs for resolver implementation

---
+++ spec/resolver/copying_fieldset_into_fieldset.spec.php
@@ -0,0 +1,24 @@
+<?php
+
+use Eno\Parser;
+
+describe('Resolution', function() {
+  describe('Copying fieldset into fieldset', function() {
+    given('input', function() {
+      return <<<DOC
+languages:
+eno = error notation
+json = json object notation
+
+copy < languages
+eno = eno notation
+DOC;
+    });
+
+    it('works as specified', function() {
+      $document = Parser::parse($this->input);
+
+      expect($document->raw())->toMatchSnapshot('spec/resolver/snapshots/copying_fieldset_into_fieldset.snap.json');
+    });
+  });
+});
