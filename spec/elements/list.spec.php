2424be9fbf77aacfb39853d80c27d8e7c07a19c4
Simon Repp <simon@fdpl.io>
2018-10-30 17:23:48 +0100

    Add complete list element specs

---
+++ spec/elements/list.spec.php
@@ -0,0 +1,175 @@
+<?php
+
+use Eno\{Field, ListElement, Section};
+
+describe('List', function() {
+  beforeAll(function() {
+    $this->_context = (object) [];
+    $this->instruction = (object) [
+      'name' => 'languages',
+      'subinstructions' => [(object) [
+        'name' => 'languages',
+        'subinstructions' => [],
+        'type' => 'LIST_ITEM',
+        'value' => 'eno'
+      ], (object) [
+        'name' => 'languages',
+        'subinstructions' => [],
+        'type' => 'LIST_ITEM',
+        'value' => 'json'
+      ], (object) [
+        'name' => 'languages',
+        'subinstructions' => [],
+        'type' => 'LIST_ITEM',
+        'value' => 'yaml'
+      ]]
+    ];
+    $this->section_instruction = (object) [
+      'depth' => 0,
+      'name' => 'mock',
+      'subinstructions' => []
+    ];
+    $this->parent = new Section($this->_context, $this->section_instruction);
+  });
+
+  beforeEach(function() {
+    $this->list = new ListElement($this->_context, $this->instruction, $this->parent);
+  });
+
+  it('is untouched after initialization', function() {
+    expect($this->list->touched)->toBe(false);
+  });
+
+  it('has only untouched items after initialization', function() {
+    foreach($this->list->elements() as $item) {
+      expect($item->touched)->toBe(false);
+    }
+  });
+
+  describe('elements()', function() {
+    beforeEach(function() {
+      $this->result = $this->list->elements();
+    });
+
+    it('touches the list itself', function() {
+      expect($this->list->touched)->toBe(true);
+    });
+
+    it('does not touch the list items', function() {
+      foreach($this->list->items([ 'elements' => true ]) as $item) {
+        expect($item->touched)->toBe(false);
+      }
+    });
+  });
+
+  describe('items()', function() {
+    describe('without a loader', function() {
+      beforeEach(function() {
+        $this->result = $this->list->items();
+      });
+
+      it('returns the values', function() {
+        expect($this->result)->toEqual(['eno', 'json', 'yaml']);
+      });
+
+      it('touches the list itself', function() {
+        expect($this->list->touched)->toBe(true);
+      });
+
+      it('touches all list items', function() {
+        foreach($this->list->elements() as $item) {
+          expect($item->touched)->toBe(true);
+        }
+      });
+    });
+
+    describe('with a loader', function() {
+      beforeEach(function() {
+        // TODO: Follow through for sensible loader signature 
+        $this->result = $this->list->items(function($context, $name, $value) { return strtoupper($value); });
+      });
+
+      it('returns the processed values', function() {
+        expect($this->result)->toEqual(['ENO', 'JSON', 'YAML']);
+      });
+
+      it('touches the element', function() {
+        expect($this->list->touched)->toBe(true);
+      });
+
+      it('touches all list items', function() {
+        foreach($this->list->elements() as $item) {
+          expect($item->touched)->toBe(true);
+        }
+      });
+    });
+
+    describe("with [ 'with_elements' => true ]", function() {
+      beforeEach(function() {
+        $this->result = $this->list->items([ 'with_elements' => true ]);
+      });
+
+      it('returns the elements', function() {
+        foreach($this->result as $item) {
+          expect($item['element'] instanceof Field)->toBe(true);
+        }
+      });
+
+      it('returns the values', function() {
+        expect(array_map(function($item) { return $item['value']; }, $this->result))->toEqual(['eno', 'json', 'yaml']);
+      });
+    });
+  });
+
+  describe('length()', function() {
+    it('returns the number of items', function() {
+      expect($this->list->length())->toBe(3);
+    });
+  });
+
+  describe('raw()', function() {
+    it('returns a native object representation', function() {
+      expect($this->list->raw())->toEqual([ 'languages' => ['eno', 'json', 'yaml'] ]);
+    });
+  });
+
+  describe('__toString()', function() {
+    it('returns a debug abstraction', function() {
+      expect((string)$this->list)->toEqual('[List name="languages" items=3]');
+    });
+  });
+
+  describe('touch()', function() {
+    describe('without options', function() {
+      beforeEach(function() {
+        $this->list->touch();
+      });
+
+      it('touches the list itself', function() {
+        expect($this->list->touched)->toBe(true);
+      });
+
+      it('does not touch the list items', function() {
+        foreach($this->list->elements() as $item) {
+          expect($item->touched)->toBe(false);
+        }
+      });
+    });
+
+    describe('with { items: true }', function() {
+      beforeEach(function() {
+        $this->list->touch([ 'items' => true ]);
+      });
+
+      it('touches the list itself', function() {
+        expect($this->list->touched)->toBe(true);
+      });
+
+      it('touches the list items', function() {
+        foreach($this->list->elements() as $item) {
+          expect($item->touched)->toBe(true);
+        }
+      });
+    });
+  });
+});
