6cbb84405e02aafa2baa89c9845d3181a1948e8b
Simon Repp <simon@fdpl.io>
2018-10-31 09:40:50 +0100

    Add specs for excessName validation error generator

---
+++ spec/errors/validation/excess_name.spec.php
@@ -0,0 +1,33 @@
+<?php
+
+use Eno\{Parser};
+
+describe('Validation::excessName', function() {
+  beforeAll(function() {
+    $this->document = Parser::parse('language: eno');
+  });
+
+  describe('without a custom message', function() {
+    beforeAll(function() {
+      $this->error = interceptValidationError(function() {
+        $this->document->assertAllTouched();
+      });
+    });
+
+    it('throws the expected error', function() {
+      expect($this->error)->toMatchErrorSnapshot('spec/errors/validation/snapshots/excess_name_without_a_custom_message.snap.error');
+    });
+  });
+
+  describe('with a custom message', function() {
+    beforeAll(function() {
+      $this->error = interceptValidationError(function() {
+        $this->document->assertAllTouched('my custom message');
+      });
+    });
+
+    it('throws the expected error', function() {
+      expect($this->error)->toMatchErrorSnapshot('spec/errors/validation/snapshots/excess_name_with_a_custom_message.snap.error');
+    });
+  });
+});
