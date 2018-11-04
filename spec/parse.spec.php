0362cd731efa1bc6d59cb0f2d12de307b48c27b2
Simon Repp <simon@fdpl.io>
2018-11-04 14:16:24 +0100

    Add specs for invalid parse calls

---
+++ spec/parse.spec.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+use Eno\{ParseError, Parser};
+
+describe('Parser', function() {
+  describe('parse()', function() {
+    it('works in a basic scenario', function() {
+      $document = Parser::parse('language: eno');
+      expect($document->field('language'))->toEqual('eno');
+    });
+
+    it('does not accept non-string input', function() {
+      $closure = function() {
+        Parser::parse(42);
+      };
+
+      expect($closure)->toThrow(new TypeError());
+    });
+
+    it('does not accept unavailable locales', function() {
+      $closure = function() {
+        Parser::parse('language: eno', [ 'locale' => 'invalid' ]);
+      };
+
+      expect($closure)->toThrow(new OutOfRangeException());
+    });
+  });
+});
